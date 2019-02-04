1cf82243ee94f3d40435dc3079b1b41740d9ff79
Konstantin Bryukhanov <kvbryukhanov@gmail.com>
2019-02-04 22:09:30 +0700

    04.02

--- SimpleDotComGame.java
+++ SimpleDotComGame.java
@@ -21,24 +21,27 @@
 //КОНЕЦ ЦИКЛА
 //КОНЕЦ МЕТОДА
   
-  public staitic void main (String[] args) {
-  
-	int numOfGuesses = 0;
-	GameHelper helper = new GameHelper();
-	
-	SimpleDotCom theDotCom = new SimpleDotCom();
-	int randomNum = (int) (Math.random() * 5);
-	
-	int[] location = {randomNum, randomNum + 1, randomNum + 2};
-	theDotCom.setLocationCells(location);
-	boolean isAlive = true;
-	
-	while (isAlive == true) {
-		String guess = helper.getUserInput("Введите число");
-		String result = theDotCom.checkYourSelf(guess);
-		if (result.equals("Потопил")){
-			isAlive = false;
-			System.out.println("Вам потребовалось" + numOfGuesses + "попыток(и)");
-		}//конец IF
-	}//конец WHILE
-  }//конец МЕТОДА MAIN
+  public class SimpleDotComGame {
+	  public static void main(String[] args) {
+	  
+		int numOfGuesses = 0;
+		GameHelper helper = new GameHelper();
+		
+		SimpleDotCom theDotCom = new SimpleDotCom();
+		int randomNum = (int) (Math.random() * 5);
+		
+		int[] location = {randomNum, randomNum + 1, randomNum + 2};
+		theDotCom.setLocationCells(location);
+		boolean isAlive = true;
+		
+		while (isAlive == true) {
+			String guess = helper.getUserInput("Введите число");
+			String result = theDotCom.checkYourSelf(guess);
+			numOfGuesses++;
+			if (result.equals("Потопил")){
+				isAlive = false;
+				System.out.println("Вам потребовалось" + numOfGuesses + "попыток(и)");
+			}//конец IF
+		}//конец WHILE
+	  }//конец МЕТОДА MAIN
+  }
